b1c93be6f8b53f248bef916b5e6cc98ac19ff405
masterscode <ogbinakaemmanuel@gmail.com>
2021-06-16 12:43:30 +0100

    added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React  from "react";
+import React from "react";
 import Auth from "./pages/auth/Auth";
 import Header from "./pages/partials/Header";
+// import { Box } from "@material-ui/system";
 
-
-const App = ()=>{
-   return <>
-   <Header/>
-      <Auth/>
-   </>
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Auth />
+    </>
+  );
+};
 
 export default App;
--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
-import {Grid, Typography} from '@material-ui/core';
+import {Grid, Typography, Box} from '@material-ui/core';
+
 const Auth=()=>{
     return (
         <Fragment>
@@ -10,6 +11,7 @@ const Auth=()=>{
             <Grid container justify='space-evenly'>
                 <Grid item xs = {8}>
                 <Typography variant='h3'>Hello there!</Typography>
+                <Box fontWeight='fontWeightBold' fontFamily='fontFamilySans'>You are about to be Boxed!</Box>
                 <Typography variant='body1' gutterBottom>Please sign in or create account to continue</Typography>
                 </Grid>
             </Grid>
